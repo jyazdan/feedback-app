8c76ea22c48c121eca4515c4dd92cf96847d0d24
Jonathan Yazdanpanah <75292585+jyazdan@users.noreply.github.com>
2022-03-09 14:03:38 -0500

    Feedback UI built with react.js

---
+++ src/components/RatingSelect.jsx
@@ -0,0 +1,38 @@
+import { useState, useContext, useEffect } from "react";
+import FeedbackContext from "../context/FeedbackContext";
+
+function RatingSelect({select}) {
+  const [selected, setSelected] = useState(10)
+  const { feedbackEdit } = useContext(FeedbackContext);
+
+  useEffect(() => {
+    setSelected(feedbackEdit.item.rating)
+  }, [feedbackEdit])
+
+  const handleChange = (event) => {
+    setSelected(+event.currentTarget.value);
+    select(+event.currentTarget.value)
+  } 
+
+  const radioButtons = [1,2,3,4,5,6,7,8,9,10]
+
+  return (
+     <ul className='rating'>
+            {radioButtons.map((num, index) => (
+                <li key={index}>
+                    <input 
+                    type='radio'
+                    id={`num${num}`}
+                    name='rating'
+                    value={`${num}`}
+                    onChange={handleChange}
+                    checked={selected === num}
+                    />
+                    <label htmlFor={`num${num}`}>{`${num}`}</label>
+                </li>
+            ))}
+        </ul>
+  )
+}
+
+export default RatingSelect;
